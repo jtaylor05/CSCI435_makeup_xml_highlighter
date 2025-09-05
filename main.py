00aac09d7d2cc2f0556231c56e00ec75211d3f87
Jackson Taylor <jacksontay666@gmail.com>
2025-09-05 13:36:49 -0400

    modified drawing method to draw dashed rectangles

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_leaves(node, leaves):
     for child in node:
         get_leaves(child, leaves)
 
-def draw_highlights(directory, file_name, save_directory = ""):
+def draw_highlights(directory, file_name, save_directory = "", line_fill=(255, 255, 40), dash_length = 5, gap = 10, w = 15):
     tree = ET.parse(os.path.join(directory, file_name) + ".xml")
     root = tree.getroot()
     leaves = []
@@ -24,10 +24,15 @@ def draw_highlights(directory, file_name, save_directory = ""):
             bounds = leaf.attrib["bounds"]
             print(leaf.attrib["class"])
             bounds = bounds.replace("][", ",")[1:-1].split(",")
-            for i in range(len(bounds)):
-                bounds[i] = int(bounds[i])
-            
-            d.rectangle(bounds, outline = (255, 255, 40), width = 4)
+            bounds = [int(x) for x in bounds]
+            for x in range(bounds[0], bounds[2], dash_length + gap):
+                d.line([(x, bounds[1]), (x + dash_length, bounds[1])],fill = line_fill, width = w) 
+            for x in range(bounds[0], bounds[2], dash_length + gap):
+                d.line([(x, bounds[3]), (x + dash_length, bounds[3])],fill = line_fill, width = w) 
+            for y in range(bounds[1], bounds[3], dash_length + gap):
+                d.line([(bounds[0], y), (bounds[0], y + dash_length)],fill = line_fill, width = w) 
+            for y in range(bounds[1], bounds[3], dash_length + gap):
+                d.line([(bounds[2], y), (bounds[2], y + dash_length)],fill = line_fill, width = w) 
 
         base.save(os.path.join(save_directory, file_name) + ".highlight.png")
 
