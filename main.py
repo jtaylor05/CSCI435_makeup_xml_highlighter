772340ef65afbd3eaf0230125a2256365b0c5d9c
Jackson Taylor <jacksontay666@gmail.com>
2025-09-05 13:22:28 -0400

    version 0.5, full working prototype

--- main.py
+++ main.py
@@ -11,37 +11,25 @@ def get_leaves(node, leaves):
     for child in node:
         get_leaves(child, leaves)
 
-def draw_highlights(file_path):
-    tree = ET.parse(file_path + ".xml")
+def draw_highlights(directory, file_name, save_directory = ""):
+    tree = ET.parse(os.path.join(directory, file_name) + ".xml")
     root = tree.getroot()
     leaves = []
     get_leaves(root, leaves)
     
-    with Image.open(file_path + ".png").convert("RGBA") as base:
+    with Image.open(os.path.join(directory, file_name) + ".png").convert("RGBA") as base:
         d = ImageDraw.Draw(base)
         
         for leaf in leaves:
             bounds = leaf.attrib["bounds"]
             print(leaf.attrib["class"])
-            bounds = bounds.replace("][", ",")
-            bounds = bounds.replace("]", "")
-            bounds = bounds.replace("[", "")
-            bounds = bounds.split(",")
+            bounds = bounds.replace("][", ",")[1:-1].split(",")
             for i in range(len(bounds)):
                 bounds[i] = int(bounds[i])
             
             d.rectangle(bounds, outline = (255, 255, 40), width = 4)
 
-        base.show()
-    # for name in file_names:
-    #     tree = ET.parse(os.path.join(directory, name + ".xml"))
-    #     root = tree.getroot()
-    #     leaves = []
-    #     get_leaves(root, leaves)
-    #     print(leaves[0].attrib)
-        
-    #     with Image.open(os.path.join(directory, name + ".png")).convert("RGBA") as base:
-    #         base.show()
+        base.save(os.path.join(save_directory, file_name) + ".highlight.png")
 
 def main():
     directory = ""
@@ -57,8 +45,8 @@ def main():
     for i in range(len(all_files)):
         all_files[i] = all_files[i][:-4]
     file_names = list(set(all_files))
-    name = file_names.pop()
-    draw_highlights(os.path.join(directory, name))
+    for name in file_names:
+        draw_highlights(directory, name)
     
 
 if __name__ == "__main__":
